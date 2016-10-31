5413cbf981d5c872189971dc2dd190eff8211e15
Bohdan <biletskyboh@gmail.com>
2016-10-31 13:31:43 -0400

    deleted language field for projects

--- cultg/cgapp/forms.py
+++ cultg/cgapp/forms.py
@@ -3,10 +3,13 @@ from django_file_form.forms import FileFormMixin, UploadedFileField
 from form_utils import forms as betterforms
 from tinymce.widgets import TinyMCE
 from django.forms.widgets import HiddenInput
+## Language choices 
 
+LANGUAGES = (('English', 'English'), ('Українська', 'Українська'))
 # Form for creation of News 
 class NewsForm(FileFormMixin, betterforms.BetterForm):
     name = forms.CharField()
+    language = forms.ChoiceField(choices=LANGUAGES)
     body = forms.CharField(widget=TinyMCE(attrs={'cols': 80, 'rows': 30}), required=False)
     image = UploadedFileField(required = False)
     form_id = forms.CharField(widget = forms.HiddenInput(), required = False)
@@ -15,7 +18,7 @@ class NewsForm(FileFormMixin, betterforms.BetterForm):
     object_id = forms.CharField(widget = forms.HiddenInput(), required = False)
 
     class Meta:
-        fieldsets = [('main', {'fields': ['name',], 'legend': 'main', }),
+        fieldsets = [('main', {'fields': ['name', 'language'], 'legend': 'main', }),
 
                 ('text-area', {'fields': ['body'], 'legend': 'text-area'}),
                 ('images', {'fields': ['image'] + ['form_id', 'upload_url', 'delete_url'], 'legend': 'images'}),
@@ -24,6 +27,7 @@ class NewsForm(FileFormMixin, betterforms.BetterForm):
 class MemberForm(FileFormMixin, betterforms.BetterForm):
     first_name = forms.CharField()
     last_name = forms.CharField()
+    language = forms.ChoiceField(choices=LANGUAGES)
     description = forms.CharField(widget=forms.Textarea)
     image = UploadedFileField(required = False)
     form_id = forms.CharField(widget = forms.HiddenInput(), required = False)
@@ -32,19 +36,20 @@ class MemberForm(FileFormMixin, betterforms.BetterForm):
     object_id = forms.CharField(widget = forms.HiddenInput(), required = False)
 
     class Meta:
-        fieldsets = [('main', {'fields': ['first_name', 'last_name', ], 'legend': 'main', }),
+        fieldsets = [('main', {'fields': ['first_name', 'last_name', 'language'], 'legend': 'main', }),
                 ('text-area', {'fields': ['description'], 'legend': 'text-area'}),
                 ('images', {'fields': ['image'] + ['form_id', 'upload_url', 'delete_url'], 'legend': 'images'}),
                      ]
 
 # Form for creations of Projects
 ### Categories for projets 
-CATEGORIES = (('Дитячі Георгафії', 'Дитячі Георгафії'), ('Розвиток Територій', 'Розвиток Територій'))
+CATEGORIES = (('Children geographies', 'Children geographies'), ('Development of territories', 'Development of territories'))
 
 class ProjectForm(FileFormMixin, betterforms.BetterForm):
     name = forms.CharField()
     description = forms.CharField(widget=forms.Textarea, required = False)
     category = forms.ChoiceField(choices=CATEGORIES)
+    language = forms.ChoiceField(choices=LANGUAGES)
     image = UploadedFileField(required = False)
     form_id = forms.CharField(widget = forms.HiddenInput(), required = False)
     upload_url = forms.CharField(widget = forms.HiddenInput(), required = False)
@@ -52,13 +57,14 @@ class ProjectForm(FileFormMixin, betterforms.BetterForm):
     object_id = forms.CharField(widget = forms.HiddenInput(), required = False)
 
     class Meta:
-        fieldsets = [('main', {'fields': ['name', 'category', ], 'legend': 'main', }),
+        fieldsets = [('main', {'fields': ['name', 'category', 'language'], 'legend': 'main', }),
                 ('text-area', {'fields': ['description'], 'legend': 'text-area'}),
                 ('images', {'fields': ['image'] + ['form_id', 'upload_url', 'delete_url'], 'legend': 'images'}),
                      ]
 # Form for creations of Partners 
 class PartnerForm(FileFormMixin, betterforms.BetterForm):
     name = forms.CharField()
+    language = forms.ChoiceField(choices=LANGUAGES)
     image = UploadedFileField(required = False)
     form_id = forms.CharField(widget = forms.HiddenInput(), required = False)
     upload_url = forms.CharField(widget = forms.HiddenInput(), required = False)
@@ -66,7 +72,7 @@ class PartnerForm(FileFormMixin, betterforms.BetterForm):
     object_id = forms.CharField(widget = forms.HiddenInput(), required = False)
 
     class Meta:
-        fieldsets = [('main', {'fields': ['name',], 'legend': 'main', }),
+        fieldsets = [('main', {'fields': ['name', ], 'legend': 'main', }),
                 ('images', {'fields': ['image'] + ['form_id', 'upload_url', 'delete_url'], 'legend': 'images'}),
                      ]
 

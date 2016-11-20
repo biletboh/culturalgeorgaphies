ede8711ae3ee82c3add6272764525f2dbddf9ce2
Bohdan <biletskyboh@gmail.com>
2016-11-20 16:01:49 -0500

    added Member and Project to admin

--- cultg/cgapp/admin.py
+++ cultg/cgapp/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import News 
+from .models import News, Member, Project 
 
 class ArticleAdmin(admin.ModelAdmin):
     class Media:
         js = ['/path/to/tinymce/jscripts/tiny_mce/tiny_mce.js',
             '/path/to/your/tinymce_setup.js']
 
-admin.site.register(News, ArticleAdmin)
+admin.site.register(News, Member, Project, ArticleAdmin)

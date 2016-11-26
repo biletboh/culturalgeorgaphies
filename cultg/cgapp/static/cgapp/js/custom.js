e916fc76eba08a5d28bcac94a36f0b6b6f9b363b
Bohdan <biletskyboh@gmail.com>
2016-11-26 04:57:13 -0500

    update about page

---
+++ cultg/cgapp/static/cgapp/js/custom.js
@@ -0,0 +1,11 @@
+(function($) {
+    "use strict"; // Start of use strict
+    // Initialize and Configure Scroll Reveal Animation
+    window.sr = ScrollReveal();
+    sr.reveal('.sr-icons', {
+        duration: 1000,
+        scale: 0.3,
+        distance: '0px'
+    }, 200);
+
+})(jQuery)// End of use strict

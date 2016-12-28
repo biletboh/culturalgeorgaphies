2412b47e6ea6977f4e61b1737cf598c16c829bc0
biletboh <biletskyboh@gmail.com>
2016-12-28 14:14:27 +0200

    added responsive styles to tinymce

--- cultg/cultg/settings.py
+++ cultg/cultg/settings.py
@@ -217,10 +217,13 @@ TINYMCE_JS_ROOT = '/static/tiny_mce'
 
 TINYMCE_SPELLCHECKER = True
 TINYMCE_DEFAULT_CONFIG = {
-        'plugins': 'advlist,autolink,emotions,fullpage,fullscreen,media,table,spellchecker,paste,searchreplace,wordcount',
+        'plugins': 'advlist,autolink,autoresize,emotions,fullpage,fullscreen,media,table,spellchecker,paste,searchreplace,wordcount',
         'theme': "advanced",
-        'theme_advanced_buttons1': 'undo,redo,|,fontselect,fontsizeselect,|,bold,italic,underline,strikethrough,|,forecolor,backcolor,|,bullist,numlist,|,justifyleft,justifycenter,justifyright,justifyfull,|,outdent,indent,',
-        'theme_advanced_buttons2' : "link,unlink,|,image,media,|,emotions,|,blockquote,|,table,|,hr,sub,sup,|,charmap",
+        'theme_advanced_resizing': True,
+        'theme_advanced_resize_horizontal': True,
+        'theme_advanced_buttons1': 'undo,redo,|,fontselect,fontsizeselect,|,bold,italic,underline,strikethrough,|,forecolor,backcolor,|,bullist,numlist,|,justifyleft,justifycenter,justifyright,justifyfull,|,outdent,indent,|,link,unlink,|,image,media,|,emotions,|,blockquote,|,table,|,hr,sub,sup,|,charmap',
+        'theme_advanced_buttons2' : "",
+        'width': '100%',
         'cleanup_on_startup': True,
         'custom_undo_redo_levels': 10,
         }

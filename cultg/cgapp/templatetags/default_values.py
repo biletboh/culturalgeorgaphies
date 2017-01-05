7a2c39abf425e888145c8700db12574adab41044
biletboh <biletskyboh@gmail.com>
2017-01-05 12:54:14 +0200

    fixed default choices

--- cultg/cgapp/templatetags/default_values.py
+++ cultg/cgapp/templatetags/default_values.py
@@ -8,7 +8,7 @@ def default_choice(field, choice):
     if choice:
         choice_id = 0
         for e in field.field.choices:
-            if e[1] == choice:
+            if e[0] == choice:
                 choice_id = e[0]
 
         field.field.initial = choice_id
